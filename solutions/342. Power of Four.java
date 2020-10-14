aaff0ec903142d077599f234d78f22c2cda7ed3d
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-13 17:16:55 -0700

    1 ms

--- solutions/342. Power of Four.java
+++ solutions/342. Power of Four.java
@@ -1,6 +1,17 @@
 class Solution {
     public boolean isPowerOfFour(int num) {
-        // solution 1
-        return (Math.log(num) / Math.log(4)) % 1 == 0;
+        // solution 1 O(Log n)
+        // if(num == 0) return false;
+        // while(num != 1){
+        //     if(num % 4 != 0) return false;
+        //     num /= 4;
+        // }
+        // return true;
+        
+        // solution 2 O(1)
+        // return (Math.log(num) / Math.log(4)) % 1 == 0;
+        
+        // solution 3 0(1)
+        return num > 0 && ((num & (num - 1)) == 0) && (num - 1) % 3 == 0;
     }
 }
