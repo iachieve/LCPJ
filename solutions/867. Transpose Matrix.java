07d449bdb66b3aa69886a7d402ce79541516d8d3
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-13 18:12:53 -0700

    0 ms

---
+++ solutions/867. Transpose Matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] transpose(int[][] A) {
+        int rows = A.length;
+        int columns = A[0].length;
+        int[][] result = new int[columns][rows];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                result[j][i] = A[i][j];
+            }
+        }
+        return result;
+    }
+}
