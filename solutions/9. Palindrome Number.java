c6edce50716dd8a4779a15af3f84d5b491da2075
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-15 05:52:16 -0700

    7 ms

--- solutions/9. Palindrome Number.java
+++ solutions/9. Palindrome Number.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0) return false;
-        int len = (int)Math.floor(Math.log10(x)) + 1;
-        int mask = (int)Math.pow(10, len - 1);
-        for(int i = 0; i < (len/2); i++){
-            int lastDigit = x % 10;
-            int firstDigit = x / mask;
-            if(lastDigit != firstDigit) return false;
-            x %= mask;
-            x /=10;
-            mask /= 100;
-        }
-        return true;
+if (x < 0) return false;
+        int len = (int) Math.log10(x) + 1;
+        int mask = (int) Math.pow(10, len - 1);
+​
+        for (int i = 0; i < len / 2; i++) {
+            int lsd = x % 10;
+            int msd = x / mask;
+​
+            if (lsd != msd) return false;
+​
+            x = x % mask;
+            x = x / 10;
+            mask = mask / 100;
+        }
+        return true;
     }
 }
