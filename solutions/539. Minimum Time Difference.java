99c7ed9a7cc69e650291f09ea31f67d773d8899b
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-15 09:39:58 -0700

    1 ms

--- solutions/539. Minimum Time Difference.java
+++ solutions/539. Minimum Time Difference.java
@@ -1,30 +1,32 @@
 class Solution {
-​
-    public int toMinutes(String time){
-        return (Integer.parseInt(time.substring(0, 2)) * 60) 
-            + Integer.parseInt(time.substring(3, 5));
+    private int toMin(String time){
+        return (Integer.parseInt(time.substring(0, 2)) * 60) + 
+            Integer.parseInt(time.substring(3, 5));
     }
     public int findMinDifference(List<String> timePoints) {
-        boolean[] seen = new boolean[1440];
+        int first = -1;
         int prev = -1;
         int minDiff = 1441;
-        int first = -1;
+        boolean[] seen = new boolean[1440];
+        
         for(String time: timePoints){
-            int min = toMinutes(time);
-            if(seen[min]) return 0;
-            seen[min] = true;
+            int n = toMin(time);
+            if(seen[n]) return 0;
+            seen[n] = true;
         }
+        
         for(int i = 0; i < 1440; i++){
             if(seen[i]){
                 if(prev != -1){
                     minDiff = Math.min(minDiff, (i - prev));
-            }else{
-                first = i;
+                }else{
+                    first = i;
+                }
+                prev = i;
             }
-            prev = i;
-            }
-         
         }
-        return Math.min(minDiff, (first + 1440 - prev));
+        
+        minDiff = Math.min(minDiff, (1440 + first - prev));
+        return minDiff;
     }
 }
