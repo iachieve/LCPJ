107ee867379623d437ec096def7a46164ac2fa21
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-15 14:06:49 -0700

    0 ms

--- solutions/48. Rotate Image.java
+++ solutions/48. Rotate Image.java
@@ -1,18 +1,16 @@
 class Solution {
-    
-    // solution 1
     public void rotate(int[][] matrix) {
         int size = matrix.length - 1;
-        for(int layer = 0; layer < (matrix.length / 2); layer++){
+        for(int layer = 0; layer < (matrix.length/2); layer++){
             for(int i = layer; i < (size - layer); i++){
                 int top = matrix[layer][i];
                 int right = matrix[i][size - layer];
-                int bottom = matrix[size - layer][size-i];
+                int bottom = matrix[size - layer][size - i];
                 int left = matrix[size - i][layer];
                 
                 matrix[layer][i] = left;
                 matrix[i][size - layer] = top;
-                matrix[size - layer][size-i] = right;
+                matrix[size - layer][size - i] = right;
                 matrix[size - i][layer] = bottom;
             }
         }
