020290e5cbfdc45f2c0a0cf2a5b56dea54a6375b
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-15 07:57:02 -0700

    3 ms

--- solutions/415. Add Strings.java
+++ solutions/415. Add Strings.java
@@ -1,23 +1,26 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        int carry = 0;
         int i = num1.length() - 1;
         int j = num2.length() - 1;
         StringBuffer sb = new StringBuffer("");
-        while(i >= 0 || j >= 0){
+        int carry = 0;
+        while(i >= 0 || j >=0){
             int sum = carry;
             if(i >= 0){
                 sum += num1.charAt(i) - '0';
                 i--;
             }
+            
             if(j >= 0){
                 sum += num2.charAt(j) - '0';
                 j--;
             }
-            sb.insert(0, sum / 10);
-            carry = sum % 10;
+            
+            sb.insert(0, sum % 10);
+            carry = sum / 10;
         }
-        if(carry > 0) sb.insert(0, carry);
+        
+        if(carry != 0) sb.insert(0, carry);
         return sb.toString();
     }
 }
