20f728f144805dab6310542d0c3a11c269b267a6
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-15 17:18:31 -0700

    20 ms

--- solutions/15. 3Sum.java
+++ solutions/15. 3Sum.java
@@ -1,15 +1,14 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        if(nums == null || nums.length == 0 || (nums.length == 1 && nums[0] == 0)) return new ArrayList();
-        
+    public List<List<Integer>> threeSum(int[] nums) {        
         Arrays.sort(nums);
-        Set<List<Integer>> allThreeSums = new HashSet<>();
+        List<List<Integer>> allThreeSums = new ArrayList<>();
         for(int i = 0; i < nums.length - 2; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip same result
             findTwoSum(allThreeSums, nums, i);
         }
-        return new ArrayList(allThreeSums);
+        return allThreeSums;
     }
-    private void findTwoSum(Set<List<Integer>> allThreeSums, int[] nums, int rootIndex){
+    private void findTwoSum(List<List<Integer>> allThreeSums, int[] nums, int rootIndex){
         int left = rootIndex + 1;
         int right = nums.length - 1;
         while(left < right){
@@ -18,6 +17,8 @@ class Solution {
                 allThreeSums.add(Arrays.asList(nums[rootIndex], nums[left], nums[right]));
                 left++;
                 right--;
+                while (left < right && nums[left] == nums[left - 1]) left++;  // skip same result
+                while (left < right && nums[right] == nums[right + 1]) right--;  // skip same result
             }else if(sum < 0){
                 left++;
             }else{
