f1ee76307619a6ec9338eda9583c909f373b0321
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-16 06:13:28 -0700

    1 ms

--- solutions/409. Longest Palindrome.java
+++ solutions/409. Longest Palindrome.java
@@ -1,15 +1,36 @@
 class Solution {
     public int longestPalindrome(String s) {
-        Set<Character> unmatchingChars = new HashSet<Character>();
-        int matches = 0;
-        for(char c: s.toCharArray()){
-            if(unmatchingChars.contains(c)){
-                matches++;
-                unmatchingChars.remove(c);
-            }else{
-                unmatchingChars.add(c);
-            }
+        
+        // ********** Integer Array Solution **********
+        /*
+        * Runtime: 1 ms, faster than 100.00% of Java online submissions for Longest Palindrome.
+        * Memory Usage: 37.2 MB, less than 16.70% of Java online submissions for Longest Palindrome.
+        */
+        int result = 0;
+        int[] counts = new int[128];
+        for(char c: s.toCharArray()) counts[c]++;
+        for(Integer cc: counts){
+            result += cc / 2 * 2;
+            if(result % 2 == 0 && cc % 2 == 1) result += 1;
         }
-        return (matches * 2) + (unmatchingChars.isEmpty() ? 0 : 1);
+        return result;
+        
+        
+        // ***** HashSet Solution *********
+        /*
+        * Runtime: 4 ms, faster than 55.85% of Java online submissions for Longest Palindrome.
+        * Memory Usage: 38.6 MB, less than 16.70% of Java online submissions for Longest Palindrome.
+        */
+        // Set<Character> unmatchingChars = new HashSet<Character>();
+        // int matches = 0;
+        // for(char c: s.toCharArray()){
+        //     if(unmatchingChars.contains(c)){
+        //         matches++;
+        //         unmatchingChars.remove(c);
+        //     }else{
+        //         unmatchingChars.add(c);
+        //     }
+        // }
+        // return (matches * 2) + (unmatchingChars.isEmpty() ? 0 : 1);
     }
 }
