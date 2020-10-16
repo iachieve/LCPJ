78639986b0b59b78b8591211f772a866e9ece503
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-15 17:29:48 -0700

    14 ms

--- solutions/36. Valid Sudoku.java
+++ solutions/36. Valid Sudoku.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         Set seen = new HashSet();
-        for(int r = 0; r < 9; r++){
-            for(int c = 0; c < 9; c++){
-                char curr = board[r][c];
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j <9; j++){
+                char curr = board[i][j];
                 if(curr != '.'){
-                    if(
-                    !seen.add(curr + " in row " + r) ||
-                    !seen.add(curr + " in col " + c) ||
-                    !seen.add(curr + " in box " + r/3 + "-" + c/3)
-                ){
-                    return false;
-                }
-                }
-            }   
+                   if(!seen.add(curr + " found in row " + i)||
+                     !seen.add(curr + " found in col" + j)||
+                     !seen.add(curr + " found in box " + (i/3) +"-"+(j/3))){
+                       return false;
+                   } 
+                }
+            }
         }
-        
         return true;
-        
     }
 }
