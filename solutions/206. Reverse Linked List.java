124b72448c18f82e34707a1935f9377a88a7360a
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-17 08:11:15 -0700

    0 ms

--- solutions/206. Reverse Linked List.java
+++ solutions/206. Reverse Linked List.java
@@ -9,26 +9,25 @@
  * }
  */
 ​
-// 8 / 20
 class Solution {
     /*
     Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
     Memory Usage: 38.8 MB, less than 6.65% of Java online submissions for Reverse Linked List.
     */
-        public ListNode reverseList(ListNode head) {
-          if(head == null || head.next == null) return head;
-            ListNode ret = reverseList(head.next);
-            head.next.next = head;
-            head.next = null;
-            return ret;
-        }
-    
-    // save next
-    // reverse current next to prev
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode temp = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return temp;
+    }
+ 
+    // save next listnode
+    // reverse current node prev
     // advance prev
     // advance curr
     public ListNode reverseListIterative(ListNode head) {
-       ListNode curr = head, prev = null, next = null;
+        ListNode curr = head, prev = null, next = null;
         while(curr != null){
             next = curr.next;
             curr.next = prev;
