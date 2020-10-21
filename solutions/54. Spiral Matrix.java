93ae254c7a4f5f28b46127be7ccba0c9b99b763a
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-20 19:30:23 -0700

    0 ms

--- solutions/54. Spiral Matrix.java
+++ solutions/54. Spiral Matrix.java
@@ -1,35 +1,24 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> nums = new ArrayList<Integer>();
-        if (matrix == null || matrix.length == 0) return nums;
-​
-        int size = matrix.length * matrix[0].length - 1;
-        int top = 0;
-        int bottom = matrix.length - 1; // last row index
-        int left = 0;
-        int right = matrix[0].length - 1; // last column index
-        
-        while(left <=right && top <= bottom){
-            for(int i = left; i <= right && nums.size() <= size; i++){
-                nums.add(matrix[top][i]);
+        List<Integer> res = new ArrayList<>();
+        if(matrix.length == 0) return res;
+        int sr = 0, er = matrix.length-1, sc= 0, ec = matrix[0].length-1;
+        while(sr <= er && sc <= ec){
+            for(int i= sc  ; i <= ec; i++) res.add(matrix[sr][i]);
+            for(int i= sr+1; i <=  er; i++) res.add(matrix[i][ec]);
+            for(int i= ec-1; i >= sc ; i--) {
+                if(sr == er) break;
+                res.add(matrix[er][i]);
             }
-            top++;
-  
-            for(int i = top; i<= bottom && nums.size() <= size; i++){
-                nums.add(matrix[i][right]);
+            for(int i= er-1; i >= sr+1 ; i--) {
+                if(sc == ec) break;
+                res.add(matrix[i][sc]);
             }
-            right--;
-            
-            for(int i = right; i >= left && nums.size() <= size; i--){
-                nums.add(matrix[bottom][i]);
-            }
-            bottom--;
-            
-            for(int i = bottom; i >= top && nums.size() <= size; i--){
-                nums.add(matrix[i][left]);
-            }
-            left++;
+            sr++;
+            er--;
+            sc++;
+            ec--;
         }
-        return nums;
+        return res;
     }
 }
