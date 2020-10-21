a65156a49ad8b12293fe0aa2556bc64ac93bd1e1
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-21 13:43:39 -0700

    Update 430. Flatten a Multilevel Doubly Linked List.java

--- solutions/430. Flatten a Multilevel Doubly Linked List.java
+++ solutions/430. Flatten a Multilevel Doubly Linked List.java
@@ -1,60 +1,179 @@
 /*
 // Definition for a Node.
 class Node {
-    public int val;
-    public Node prev;
-    public Node next;
-    public Node child;
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
 };
 */
 class Solution {
-    
-    public Node flatten(Node head) {
-        if(head == null) return head;
-        Node curr = head;
-        //-----------------
-        
-      while(curr != null){
-          if(curr.child != null){
-              Node save = curr.next; // nullable
-          
-              curr.next = flatten(curr.child);
-              curr.next.prev = curr;
-              curr.child = null;
-​
-              while(curr.next != null) curr = curr.next; // get the tail
-              curr.next = save; // forgotten
-              if(save != null) save.prev = curr;
-          }
-          
-          curr = curr.next;
-      }
-        
-        //-----------------
-        return head;
-    }
-​
-    
+    
+    public Node flatten(Node head) {
+        if(head == null) return head;
+        Node curr = head;
+        //-----------------
+        
+      while(curr != null){
+          if(curr.child != null){
+              Node save = curr.next; // nullable
+          
+              curr.next = flatten(curr.child);
+              curr.next.prev = curr;
+              curr.child = null;
+
+              while(curr.next != null) curr = curr.next; // get the tail
+              curr.next = save; // forgotten
+              if(save != null) save.prev = curr;
+          }
+          
+          curr = curr.next;
+      }
+        
+        //-----------------
+        return head;
+    }
+
+    
  
-​
-        
-    
-    
-     public Node flattenIterative(Node head) {
-       if(head == null) return head;
-        Node curr = head;
-        while(curr != null){
-            if(curr.child == null){
-                curr = curr.next;
-                continue;
-            }
-            Node tail = curr.child;
-            while(tail.next != null) tail = tail.next;
-           
-            tail.next = curr.next;
-            if(curr.next != null) curr.next.prev = tail;
-            
-            curr.next = curr.child;
-            curr.child.prev = curr;
-            curr.child = null;
-            
+
+        
+    
+    
+     public Node flattenIterative(Node head) {
+       if(head == null) return head;
+        Node curr = head;
+        while(curr != null){
+            if(curr.child == null){
+                curr = curr.next;
+                continue;
+            }
+            Node tail = curr.child;
+            while(tail.next != null) tail = tail.next;
+           
+            tail.next = curr.next;
+            if(curr.next != null) curr.next.prev = tail;
+            
+            curr.next = curr.child;
+            curr.child.prev = curr;
+            curr.child = null;
+            
+            curr = curr.next;
+        }
+        return head;
+        
+    }
+}
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
