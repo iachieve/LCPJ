e60ada2d6c4d94def41c506435c8abf8a7c23d20
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-10-31 08:50:53 -0700

    1 ms

--- solutions/98. Validate Binary Search Tree.java
+++ solutions/98. Validate Binary Search Tree.java
@@ -1,46 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-// iterative solution
 ​
 class Solution {
-  public boolean isValidBST(TreeNode root) {
-    Stack<TreeNode> stack = new Stack();
-    double inorder =  - Double.MAX_VALUE;
+//     public boolean isValidBST(TreeNode root) {
+//         return isValidBST(root, null, null);
+//     }
+    
+//     private boolean isValidBST(TreeNode root, Integer min, Integer max) {
+//         if(root == null) return true;
+//         if((min != null && root.val <= min) || (max != null && root.val >= max)) return false;
+//         return isValidBST(root.left, min, root.val) &&
+//             isValidBST(root.right, root.val, max);
+//     }
+    
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack();
+        double inorder = - Double.MAX_VALUE;
 ​
-    while (!stack.isEmpty() || root != null) {
-      while (root != null) {
-        stack.push(root);
-        root = root.left;
-      }
-      root = stack.pop();
-      // If next element in inorder traversal
-      // is smaller than the previous one
-      // that's not BST.
-      if (root.val <= inorder) return false;
-      inorder = root.val;
-      root = root.right;
-    }
-    return true;
+        while (!stack.isEmpty() || root != null) {
+          while (root != null) {
+            stack.push(root);
+            root = root.left;
+          }
+          root = stack.pop();
+          if (root.val <= inorder) return false;
+          inorder = root.val;
+          root = root.right;
+        }
+        return true;
   }
 }
-​
-/*
-// recursive solution
-class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, null, null);
-    }
-    
