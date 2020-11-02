5ff20d85c689e6f557d61230708476df1e3950b1
iachieve <mhalim80@hotmail.com>
2020-11-02 05:30:30 -0800

    check power of any number mathimatically in O(1)

--- solutions/check-Power-of-any-number-mathimatically.java
+++
@@ -1,6 +0,0 @@
-public static void main(String[] args) {
-        System.out.println(isPowerOf(36, 6));
-    }
-    public static boolean isPowerOf(int num, int powerOf) {
-        return (Math.log(num) / Math.log(powerOf)) % 1 == 0;
-    }
