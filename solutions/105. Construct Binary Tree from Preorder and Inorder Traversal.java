27217d5f8985d5f71f636d297865ac0efd27a679
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-02 15:42:53 -0800

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.java
    using hashmap

--- solutions/105. Construct Binary Tree from Preorder and Inorder Traversal.java
+++ solutions/105. Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -1,30 +1,61 @@
+
+// Runtime: 1 ms, faster than 98.38% of Java online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+// Memory Usage: 39.1 MB, less than 5.63% of Java online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+
 class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper(0, 0, inorder.length - 1, preorder, inorder);
-    }
-​
-    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
-​
-        if (preStart > preorder.length || inStart > inEnd){
-            return null;
-        }
-        
-        // 2. Build the current node.
-        TreeNode cur = new TreeNode(preorder[preStart]);
-        
-        // 3. Find the pivot in the inorder.
-        int inPivot = 0;
-        for (int i = 0; i < inorder.length; i++){
-            if (inorder[i] == preorder[preStart]){
-                inPivot = i;
-                break;
-            }
-        }
-        
-        // 4. return root.
-        cur.left  = helper(preStart + 1, inStart, inPivot - 1, preorder, inorder);
-        // key
-        cur.right = helper(preStart + inPivot - inStart + 1, inPivot + 1, inEnd, preorder, inorder);
-        return cur;
-    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> inorderMap = getValueToIndex(inorder);
+        return helper(0, 0, inorder.length - 1, preorder, inorderMap);
+    }
+
+    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, Map<Integer, Integer> inorder) {
+        if (preStart > preorder.length || inStart > inEnd) return null;
+
+        TreeNode cur = new TreeNode(preorder[preStart]);
+        int inPivot = inorder.get(preorder[preStart]);
+        cur.left  = helper(preStart + 1, inStart, inPivot - 1, preorder, inorder);
+        cur.right = helper(preStart + inPivot - inStart + 1, inPivot + 1, inEnd, preorder, inorder);
+        return cur;
+    }
+    
+      private Map<Integer, Integer> getValueToIndex(int[] inorder) {
+        Map<Integer, Integer> valueToIndex = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++) {
+            valueToIndex.put(inorder[i], i);
+        }
+        return valueToIndex;
+    }    
 }
+
+
+
+/*
+// Runtime: 3 ms, faster than 53.56% of Java online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+// Memory Usage: 38.9 MB, less than 5.63% of Java online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(0, 0, inorder.length - 1, preorder, inorder);
+    }
+
+    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart > preorder.length || inStart > inEnd) return null;
+
+        TreeNode cur = new TreeNode(preorder[preStart]);
+        
+        // duplicate work happening here
+        int inPivot = 0;
+        for (int i = 0; i < inorder.length; i++){
+            if (inorder[i] == preorder[preStart]){
+                inPivot = i;
+                break;
+            }
+        }
+        
+        cur.left  = helper(preStart + 1, inStart, inPivot - 1, preorder, inorder);
+        cur.right = helper(preStart + inPivot - inStart + 1, inPivot + 1, inEnd, preorder, inorder);
+        return cur;
+    }
+}
+
+*/
