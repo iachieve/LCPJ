6dc94916d6fc129fd82f09fb0de84dfb6d1b788b
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-03 09:51:45 -0800

    0 ms

---
+++ solutions/701. Insert into a Binary Search Tree.java
@@ -0,0 +1,26 @@
+class Solution {
+    // iterative solution
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
+        
+        TreeNode currNode = root;
+        while(true){
+            if(currNode.val <= val){
+                if(currNode.right != null){
+                    currNode = currNode.right;
+                }else{
+                    currNode.right = new TreeNode(val);
+                    break;
+                }
+            }else{
+                if(currNode.left != null){
+                    currNode = currNode.left;
+                } else{
+                    currNode.left = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+}
