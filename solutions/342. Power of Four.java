3c249fdc5e3dc82fbdcdea92a1a69afab53416ce
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-02 18:45:19 -0800

    Update 342. Power of Four.java
    general formula to calculate if any given number is power of any number (Math.log10(x) / Math.log10(y)) % 1 == 0;

--- solutions/342. Power of Four.java
+++ solutions/342. Power of Four.java
@@ -1,44 +1,47 @@
 class Solution {
-    public boolean isPowerOfFour(int n) {
-        int alternatingOddBitMask = 0x55555555; // 1010101010101010101010101010101
-        boolean isNonZero = (n != 0);
-        boolean hasSingleLeadingOneBit = (n & (n - 1)) == 0;
-        boolean hasOnlyOddPositionedBits = (n & alternatingOddBitMask) == n;
-​
-        return isNonZero && hasSingleLeadingOneBit && hasOnlyOddPositionedBits;
-    }
+    public boolean isPowerOfFour(int n) {
+//         int alternatingOddBitMask = 0x55555555; // 1010101010101010101010101010101
+//         boolean isNonZero = (n != 0);
+//         boolean hasSingleLeadingOneBit = (n & (n - 1)) == 0;
+//         boolean hasOnlyOddPositionedBits = (n & alternatingOddBitMask) == n;
+
+//         return isNonZero && hasSingleLeadingOneBit && hasOnlyOddPositionedBits;
+        
+                return (Math.log10(n) / Math.log10(4)) % 1 == 0;
+
+    }
 }
-​
+
 /*
-​
-​
-​
+
+
+
 ```
-​
+
 ```
-​
-​
+
+
 ```
-    public boolean isPowerOfFour(int n) {
-       return (Math.log(n) / Math.log(4)) % 1 == 0;
-    }
+    public boolean isPowerOfFour(int n) {
+       return (Math.log(n) / Math.log(4)) % 1 == 0;
+    }
 ```
-​
+
 ```
-    public boolean isPowerOfFour(int n) {
-        if(n == 0) return false;
-        while(n != 1){
-            if(n % 4 != 0) return false;
-            n /= 4;
-        }
-        return true;
-    }
+    public boolean isPowerOfFour(int n) {
+        if(n == 0) return false;
+        while(n != 1){
+            if(n % 4 != 0) return false;
+            n /= 4;
+        }
+        return true;
+    }
 }
 ```
-​
+
 ```
-   public boolean isPowerOfFour(int n) {
-        return ((n & (n-1)) == 0 && (n % 3) == 1);
-    }
+   public boolean isPowerOfFour(int n) {
+        return ((n & (n-1)) == 0 && (n % 3) == 1);
+    }
 ``` 
 */
