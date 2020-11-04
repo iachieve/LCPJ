2049579b72311e4cbc79cc0c7651185d83f4fc1a
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-04 08:34:01 -0800

    Update 297. Serialize and Deserialize Binary Tree.java
    297. Serialize and Deserialize Binary Tree

--- solutions/297. Serialize and Deserialize Binary Tree.java
+++ solutions/297. Serialize and Deserialize Binary Tree.java
@@ -1,9 +1,39 @@
 public class Codec {
+    public String serialize(TreeNode root) {
+        if (root == null) return "#";
+        
+        StringBuilder sb = new StringBuilder();
+        return sb.append(root.val)
+            .append(",")
+            .append(serialize(root.left))
+            .append(",")
+            .append(serialize(root.right))
+            .toString();
+    }
+
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return helper(queue);
+    }
+    
+    private TreeNode helper(Queue<String> queue) {
+        String s = queue.poll();
+        if (s.equals("#")) return null;
+        TreeNode root = new TreeNode(Integer.valueOf(s));
+        root.left = helper(queue);
+        root.right = helper(queue);
+        return root;
+    }
+}
+
+
+/*
+public class Codec {
     public String serialize(TreeNode root) {
         if (root == null) return "#";
         return root.val + "," + serialize(root.left) + "," + serialize(root.right);
     }
-​
+
     public TreeNode deserialize(String data) {
         Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
         return helper(queue);
@@ -18,8 +48,13 @@ public class Codec {
         return root;
     }
 }
-​
+
 // Your Codec object will be instantiated and called as such:
 // Codec ser = new Codec();
 // Codec deser = new Codec();
 // TreeNode ans = deser.deserialize(ser.serialize(root));
+
+
+
+
+*/
