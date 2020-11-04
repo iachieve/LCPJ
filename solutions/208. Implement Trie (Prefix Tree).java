169863ba9718a771cbb5644e297af939b6d5304d
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-04 15:16:36 -0800

    29 ms

--- solutions/208. Implement Trie (Prefix Tree).java
+++ solutions/208. Implement Trie (Prefix Tree).java
@@ -1,14 +1,11 @@
 class Trie {
 ​
     private Node root;
-    public Trie() {
-        root = new Node('\0');
-    }
+    public Trie() {↔}
     
     public void insert(String word) {
         Node curr = root;
-        for(int i = 0; i < word.length(); i++){
-            char c = word.charAt(i);
+        for(char c :word.toCharArray()){
             if(curr.children[c-'a'] == null) curr.children[c-'a'] = new Node(c);
             curr = curr.children[c-'a'];
         }
@@ -26,10 +23,9 @@ class Trie {
     
     private Node getNode(String word){
         Node curr = root;
-        for(int i = 0; i < word.length(); i++){
-            char c = word.charAt(i);
+        for(char c : word.toCharArray()){
             if(curr.children[c-'a'] == null) return null;
-            curr = curr.children[c-'a'];
+            curr = curr.children[c - 'a'];
         }
         return curr;
     }
@@ -40,16 +36,9 @@ class Trie {
         public Node[] children;
         public Node(char c){
             this.c = c;
-            isWord = false;
-            children = new Node[26];
+            this.isWord = false;
+            this.children = new Node[26];
         }
-    }
+    }    
 }
 ​
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
