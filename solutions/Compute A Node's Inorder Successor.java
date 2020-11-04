0ab93efb496e115b95ffb175f629e0140b8ea8fe
iachieve <mhalim80@hotmail.com>
2020-11-04 10:36:31 -0800

    Compute A Node's Inorder Successor

---
+++ solutions/Compute A Node's Inorder Successor.java
@@ -0,0 +1,46 @@
+/*
+Compute A Node's Inorder Successor
+Given a binary tree and an integer value x, return the inorder successor of the node with value x.
+
+In-order Successors:
+The in-order successor of a node in binary tree is defined to be the next node in the in-order traversal of the Binary Tree.
+
+Example 1:
+Input: 
+          1 
+        /   \
+      5      8
+    /  \      \
+   7    3      2
+        /       \
+       6        10
+
+x = 6
+
+Output: Node{3}
+
+Explanation: The in-order traversal of the tree is [7,5,6,3,1,8,2,10]. 3 comes after 6.
+
+Example 2:
+Input: 
+          1 
+        /   \
+      5      8
+    /  \      \
+   7    3      2
+        /       \
+       6        10
+
+x = 10
+
+Output: null
+Explanation: The in-order traversal of the tree is [7,5,6,3,1,8,2,10]. 10 is the last node.
+
+Constrains:
+A node with the value x will always exist
+All nodes will have unique values
+*/
+
+public class Compute A Node's Inorder Successor {
+  
+}
