bcad34044e544773c82670dc1dc53db3ca400d88
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-07 14:23:03 -0800

    0 ms

--- solutions/191. Number of 1 Bits.java
+++ solutions/191. Number of 1 Bits.java
@@ -3,8 +3,8 @@ public class Solution {
     public int hammingWeight(int n) {
         int c = 0;
         while (n != 0){
-            c++;
-            n &= (n-1);
+            if((n&1) == 1) c++;
+            n= n >>> 1;
         }
         return c;
     }
