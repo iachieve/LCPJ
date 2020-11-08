9d4f2bdee831ec743bf295f28a4a81f9f6619405
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-08 12:22:11 -0800

    4 ms

--- solutions/36. Valid Sudoku.java
+++ solutions/36. Valid Sudoku.java
@@ -1,40 +1,27 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set seen = new HashSet();
+        Set set = new HashSet();
+        StringBuilder rowSB = new StringBuilder();
+        StringBuilder colSB = new StringBuilder();
+        StringBuilder boxSB = new StringBuilder();
+​
         for(int i = 0; i < 9; i++){
-            for(int j = 0; j <9; j++){
-                char curr = board[i][j];
-                if(curr != '.'){
-                   if(!seen.add(curr + " found in row " + i)||
-                     !seen.add(curr + " found in col" + j)||
-                     !seen.add(curr + " found in box " + (i/3) +"-"+(j/3))){
-                       return false;
-                   } 
-                }
-            }
+            for(int c = 0; c < 9; c++){
+                rowSB.setLength(0);
+                colSB.setLength(0);
+                boxSB.setLength(0);
+                char cur = board[i][c];
+                if(cur != '.'){
+                    if(!set.add(rowSB.append(cur).append("r").append(i).toString())
+                      || !set.add(colSB.append(cur).append("c").append(c).toString())
+                       || !set.add(boxSB.append(cur).append("sq").append(i/3).append(c/3).toString())
+                      )
+                        {
+                        
+                        return false;
+                        }
+            }}
         }
         return true;
     }
 }
-​
-// public boolean isValidSudoku(char[][] board) {
-//         for(int i = 0;i < 9;i++){
-//             boolean[] rowCheck = new boolean[9];
-//             boolean[] colCheck = new boolean[9];
-//             boolean[] boxCheck = new boolean[9];
-//             for(int j = 0;j< 9;j++){
-//                 if(board[i][j] == '.'){}
-//                 else if(rowCheck[board[i][j] - '1']) return false;
-//                 else rowCheck[board[i][j] - '1'] = true;
-//                 if(board[j][i] == '.'){}
-//                 else if(colCheck[board[j][i] - '1']) return false;
-//                 else colCheck[board[j][i] - '1'] = true;
-//                 int m = i/3*3 + j/3;
-//                 int n = i%3*3 + j%3;
-//                 if(board[m][n] == '.') {}
-//                 else if(boxCheck[board[m][n] - '1']) return false;
-//                 else boxCheck[board[m][n] - '1'] = true;
-//             }
-//         }
-//         return true;
-//     }
