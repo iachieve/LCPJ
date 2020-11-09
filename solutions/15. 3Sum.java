7423393f688444541a5fcd01e900890faf0ddcb9
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-09 05:08:54 -0800

    19 ms

--- solutions/15. 3Sum.java
+++ solutions/15. 3Sum.java
@@ -1,29 +1,29 @@
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {        
-        Arrays.sort(nums);
+class Solution {    //  5   /   30
+    public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> allThreeSums = new ArrayList<>();
+        Arrays.sort(nums);
         for(int i = 0; i < nums.length - 2; i++){
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip same result
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             findTwoSum(allThreeSums, nums, i);
         }
         return allThreeSums;
     }
+                
     private void findTwoSum(List<List<Integer>> allThreeSums, int[] nums, int rootIndex){
-        int left = rootIndex + 1;
-        int right = nums.length - 1;
+        int left = rootIndex + 1, right = nums.length - 1;
         while(left < right){
             int sum = nums[rootIndex] + nums[left] + nums[right];
             if(sum == 0){
-                allThreeSums.add(Arrays.asList(nums[rootIndex], nums[left], nums[right]));
-                left++;
-                right--;
-                while (left < right && nums[left] == nums[left - 1]) left++;  // skip same result
-                while (left < right && nums[right] == nums[right + 1]) right--;  // skip same result
-            }else if(sum < 0){
-                left++;
-            }else{
-                right--;
-            }
+                allThreeSums.add(Arrays.asList(nums[rootIndex] , nums[left] , nums[right]));
+                left++; right--;
+                while(left < right && nums[left] == nums[left-1]) left++;
+                while(left < right && nums[right] == nums[right+1]) right--;
+            }else if(sum < 0) left++;
+            else right--;
         }
     }
+  
+    
+                
+                
 }
