9a97cd393fda16aa219c208ef5dddcf32c1e1213
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-11 01:20:41 -0800

    1 ms

--- solutions/290. Word Pattern.java
+++ solutions/290. Word Pattern.java
@@ -1,17 +1,27 @@
 class Solution {
-    public boolean wordPattern(String pattern, String s) {
+    // Runtime: 0 ms, faster than 100.00% of Java online submissions for Word Pattern.
+    // Memory Usage: 37 MB, less than 10.21% of Java online submissions for Word Pattern.
+    public boolean wordPattern(String patternString, String sentence) {
+        Map<Character, String> patternToWord = new HashMap<>();
         Map<String, Character> wordToPattern = new HashMap<>();
-        Map<Character, String> patternToWord = new HashMap<>();
-        String[] words = s.split(" ");
-        if (words.length != pattern.length()) return false;
-        for (int i = 0; i < words.length; i++) {
-            char patternChar = pattern.charAt(i);
-            if (!wordToPattern.containsKey(words[i])) wordToPattern.put(words[i], patternChar);
-            if (!patternToWord.containsKey(pattern.charAt(i))) patternToWord.put(patternChar, words[i]);
-            if (wordToPattern.get(words[i]) != patternChar ||
-                    !patternToWord.get(pattern.charAt(i)).equals(words[i]))
-                return false;
-        }
-        return true;
+        
+        String[] words = sentence.split(" ");
+        if(words.length != patternString.length()) return false;
+        
+        for(int i = 0; i < words.length; i++){
+            char patternChar = patternString.charAt(i);
+            String word = words[i];
+            
+            if(!patternToWord.containsKey(patternChar))
+                patternToWord.put(patternChar, word);
+            if(!wordToPattern.containsKey(word))
+                wordToPattern.put(word, patternChar);
+            
+            if(!patternToWord.get(patternChar).equals(word)
+              || wordToPattern.get(word) != patternChar
+              ) return false;
+        }
+        return true;
     }
 }
+// 1
