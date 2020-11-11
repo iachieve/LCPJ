c238763a3da064ad8119d0fa500f51e180e293bd
iachieve <mhalim80@hotmail.com>
2020-11-10 17:20:58 -0800

    Min Heap Construction

--- solutions/MinHeap/MinHeap.java
+++ solutions/MinHeap/MinHeap.java
@@ -1,10 +1,9 @@
 package MinHeap;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MinHeap implements IMinHeap {
-    public List<Integer> heap = new ArrayList<>();
+    public List<Integer> heap;
 
     public MinHeap(List<Integer> array) {
         heap = buildHeap(array);
@@ -12,58 +11,59 @@ public class MinHeap implements IMinHeap {
 
     public List<Integer> buildHeap(List<Integer> array) {
         int firstParentIndex = (array.size() - 2) / 2;
-        for (int currIdx = firstParentIndex; currIdx >= 0; currIdx++) {
+        for (int currIdx = firstParentIndex; currIdx >= 0; currIdx--) {
             siftDown(currIdx, array.size() - 1, array);
         }
         return array;
     }
 
-    public void siftDown(int currIdx, int endIdx, List<Integer> heap) {
-        int firstChildIdx = currIdx * 2 + 1;
-        while (firstChildIdx < endIdx) {
-            int secondChildIdx = currIdx * 2 + 2 < endIdx ? currIdx * 2 + 1 : -1;
+    public void siftDown(int cIdx, int endIdx, List<Integer> heap) {
+        int fIdx = cIdx * 2 + 1;
+        while (fIdx <= endIdx) { // <=
+            int sIdx = cIdx * 2 + 2 <= endIdx ? cIdx * 2 + 2 : -1; // <=
             int idxToSwap;
-            if (secondChildIdx != -1 && heap.get(secondChildIdx) < heap.get(currIdx)) {
-                idxToSwap = secondChildIdx;
+            if (sIdx != -1 && heap.get(sIdx) < heap.get(fIdx)) { // second < first
+                idxToSwap = sIdx;
             } else {
-                idxToSwap = firstChildIdx;
+                idxToSwap = fIdx;
             }
 
-            if (heap.get(idxToSwap) < heap.get(currIdx)) {
-                swap(idxToSwap, currIdx, heap);
-                currIdx = idxToSwap;
-                firstChildIdx = currIdx * 2 + 1;
+            if (heap.get(idxToSwap) < heap.get(cIdx)) {
+                swap(idxToSwap, cIdx, heap);
+                cIdx = idxToSwap;
+                fIdx = cIdx * 2 + 1;
             } else {
                 return;
             }
         }
     }
 
-    public void siftUp(int currIdx, List<Integer> heap) {
-        int parentIndex = (currIdx - 1) / 2;
-        while (heap.get(currIdx) < heap.get(parentIndex)) {
-            swap(currIdx, parentIndex, heap);
-            currIdx = parentIndex;
-            parentIndex = (currIdx - 1) / 2;
+    public void siftUp(int cIdx, List<Integer> heap) {
+        int pIdx = (cIdx - 1) / 2;
+        while (cIdx > 0 && heap.get(cIdx) < heap.get(pIdx)) { // while(currentIdx > 0 && heap.get(currentIdx) < heap.get(parentIndex))
+            swap(cIdx, pIdx, heap);
+            cIdx = pIdx;
+            pIdx = (cIdx - 1) / 2;
         }
-    }
-
-    public int remove() {
-        swap(0, heap.size()-1, heap);
-        int valueToRemove = heap.get(heap.size()-1);
-        heap.remove(heap.size()-1);
-        siftDown(0, heap.size()-1, heap);
-        return valueToRemove;
-    }
-
-    public void insert(int value) {
-        heap.add(heap.size() - 1);
-        siftUp(heap.size() - 1, heap);
     }
 
     public int peek() {
         return heap.get(0);
     }
+
+    public int remove() {
+        swap(0, heap.size() - 1, heap);
+        int valueToRemove = heap.get(heap.size() - 1);
+        heap.remove(heap.size() - 1);
+        siftDown(0, heap.size() - 1, heap);
+        return valueToRemove;
+    }
+
+    public void insert(int value) {
+        heap.add(value);
+        siftUp(heap.size() - 1, heap);
+    }
+
 }
 
 
