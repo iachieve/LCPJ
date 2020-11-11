8b15e1eeace540537b391858ce0d947a60d5ea2f
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-11 12:55:46 -0800

    0 ms

---
+++ solutions/74. Search a 2D Matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 0) return false;
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int left = 0;
+        int right = rows * columns - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            int midElement = matrix[mid/columns][mid%columns];
+            if(target == midElement){
+                return true;
+            }else if(target > midElement){
+                left = mid + 1;
+            }else if (target < midElement){
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
