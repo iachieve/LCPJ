0ab661b91d4c5c981a8b94d8421e0b942f6b2f04
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-12 09:04:40 -0800

    1 ms

--- solutions/69. Sqrt(x).java
+++ solutions/69. Sqrt(x).java
@@ -1,13 +1,13 @@
 class Solution {
     public int mySqrt(int x) {
       if(x <= 1) return x;
-      int l = 1, h = x/2;
-      while(l <= h){
-        int m = l + (h-l) / 2;
-        if(m == x/m && x % m == 0) return m;
-        else if(m <= x/m) l = m+1;
-        else h = m-1;
+      int lo = 1, hi = x/2;
+      while(lo <= hi){
+        int mid = lo + (hi - lo) / 2;
+        if( mid * mid == x) return mid;
+        else if (mid <= x / mid) lo = mid + 1;
+        else hi = mid - 1;
       }
-      return h;
+      return hi;
     }
 }
