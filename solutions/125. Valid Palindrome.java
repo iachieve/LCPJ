d1817d8c6d761341e216f8a2155f626e9652e725
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-13 08:32:07 -0800

    Update 125. Valid Palindrome.java
    Valid Palindrome

--- solutions/125. Valid Palindrome.java
+++ solutions/125. Valid Palindrome.java
@@ -1,3 +1,32 @@
+// Runtime: 2 ms, faster than 97.85% of Java online submissions for Valid Palindrome.
+// Memory Usage: 39.1 MB, less than 73.18% of Java online submissions for Valid Palindrome.
+ 
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s==null) return true;
+        int i=0, j=s.length()-1;
+        s = s.toLowerCase();
+        while(i<j){
+            while(!isDigitOrChar(s.charAt(i)) && i<j) i++;
+            while(!isDigitOrChar(s.charAt(j)) && i<j) j--;
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;j--;   
+        }
+        return true;
+    }
+    boolean isDigitOrChar(char c){
+        if( (c >= 'a' && c <='z') ||
+            (c >= '0' && c <='9'))
+            return true;
+        return false;
+    }
+}
+
+
+// Runtime: 33 ms, faster than 11.52% of Java online submissions for Valid Palindrome.
+// Memory Usage: 40 MB, less than 27.67% of Java online submissions for Valid Palindrome.
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]", " ")
