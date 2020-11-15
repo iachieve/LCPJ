2fc85bda0e537687d39da949be6137d05f6473f1
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-15 07:26:35 -0800

    14 ms

--- solutions/76. Minimum Window Substring.java
+++ solutions/76. Minimum Window Substring.java
@@ -1,46 +1,32 @@
 class Solution {
     public String minWindow(String s, String t) {
-      Map<Character, Integer> dicT = new HashMap<>();
-        for(char c: t.toCharArray()) dicT.put(c, dicT.getOrDefault(c, 0) + 1);
-        Map<Character, Integer> window = new HashMap<>();
-        int left = 0, right = 0, tMapSize = dicT.size(), min = Integer.MAX_VALUE, formed = 0;
-        String result = "";
-​
-        while (right < s.length()) {
-            // expand
+        
+        Map<Character, Integer> dic = new HashMap<>(), window = new HashMap<>();
+        for(char c: t.toCharArray()) dic.put(c, dic.getOrDefault(c, 0)+1);
+        
+        int left = 0, right = 0, min = Integer.MAX_VALUE, formed = 0;
+        String res = "";
+        
+        while(right < s.length()){
             char rightChar = s.charAt(right);
             window.put(rightChar, window.getOrDefault(rightChar, 0) + 1);
-​
-            if (dicT.containsKey(rightChar)) {
-                if (dicT.get(rightChar).intValue() == window.get(rightChar).intValue()) {
-                    formed++;
-                }
-            }
-​
-            while (formed == tMapSize && left <= right) { // contraction
+            if(dic.containsKey(rightChar) && window.get(rightChar).intValue() == dic.get(rightChar).intValue())
+                formed++;
+            
+            while(formed == dic.size() && left <= right){
                 char leftChar = s.charAt(left);
-                int windowSize = right - left + 1;
-​
-                if (windowSize < min) {
-                    min = windowSize;
-                    result = s.substring(left, right + 1);
+                int currWindowSize = right - left + 1;
+                if(currWindowSize < min){
+                    min = currWindowSize;
+                    res = s.substring(left, right + 1);
                 }
-​
                 window.put(leftChar, window.get(leftChar) - 1);
-​
-                boolean isLeftCharRequire = dicT.containsKey(leftChar);
-                if (isLeftCharRequire) {
-                    if (window.get(leftChar).intValue() < dicT.get(leftChar).intValue()) {
-                        formed--;
-                    }
-                }
-​
+                if(dic.containsKey(leftChar) && window.get(leftChar).intValue() < dic.get(leftChar).intValue())
+                    formed--;
                 left++;
             }
-​
             right++;
         }
-​
-        return result;
+        return res;
     }
 }
