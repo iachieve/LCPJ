bfa1e03b2ffbce07c5bd1f9f880ea6c4e851140b
iachieve <mhalim80@hotmail.com>
2020-11-16 13:40:03 -0800

    add sorting algorithm (bubblesort)

--- solutions/Sorting/BubbleSort.java
+++ solutions/Sorting/BubbleSort.java
@@ -7,8 +7,8 @@ public class BubbleSort {
 
   public int[] bubbleSort(int[] list) {
     for (int i = 0; i < list.length; i++) {
-      for (int j = i + 1; j < list.length - 1 - i; j++) {
-        if (list[j] < list[j + 1]) {
+      for (int j = 0; j < list.length - 1 - i; j++) {
+        if (list[j] > list[j + 1]) {
           int temp = list[j + 1];
           list[j + 1] = list[j];
           list[j] = temp;
