2e2ea247eef358527013a33d06c3fb413337c841
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-20 04:04:48 -0800

    6 ms

--- solutions/3. Longest Substring Without Repeating Characters.java
+++ solutions/3. Longest Substring Without Repeating Characters.java
@@ -1,3 +1,27 @@
+public class Solution {
+    
+        // Runtime: 7 ms, faster than 39.04% of Java online submissions for Longest Substring Without Repeating Characters.
+        // Memory Usage: 39.9 MB, less than 15.56% of Java online submissions for Longest Substring Without Repeating Characters.
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int ans = 0, left = 0, right = 0;
+        while (left < s.length() && right < s.length()){
+            if(!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
+                right++;
+                ans = Math.max(ans, right - left);
+            }else{
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return ans;
+    }
+}
+​
+​
+// correct but time lime exceed with long strings
+/*
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
@@ -5,10 +29,10 @@ class Solution {
         for(int i = 0; i < n; i++){
             for(int j = i + 1; j <= n; j++){
                 if(allUnique(s, i, j)) ans = Math.max(ans, j - i);
-            }
-        }
+            }
+        }
         return ans;
-    }
+    }
     
     boolean allUnique(String s, int start, int end){
         Set<Character> set = new HashSet<>();
@@ -16,7 +40,7 @@ class Solution {
             Character ch = s.charAt(i);
             if(set.contains(ch)) return false;
             set.add(ch);
-        }
+        }
         return true;
     }
-}
+}*/
