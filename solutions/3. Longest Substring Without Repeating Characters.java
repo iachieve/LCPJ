0cae2d95433b97e62310de9a995e40d580b0cfb2
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-20 08:39:29 -0800

    5 ms

--- solutions/3. Longest Substring Without Repeating Characters.java
+++ solutions/3. Longest Substring Without Repeating Characters.java
@@ -1,46 +1,17 @@
-public class Solution {
-    
-        // Runtime: 7 ms, faster than 39.04% of Java online submissions for Longest Substring Without Repeating Characters.
-        // Memory Usage: 39.9 MB, less than 15.56% of Java online submissions for Longest Substring Without Repeating Characters.
-    public static int lengthOfLongestSubstring(String s) {
-        Set<Character> set = new HashSet<>();
-        int ans = 0, left = 0, right = 0;
-        while (left < s.length() && right < s.length()){
-            if(!set.contains(s.charAt(right))) {
-                set.add(s.charAt(right));
-                right++;
-                ans = Math.max(ans, right - left);
-            }else{
-                set.remove(s.charAt(left));
-                left++;
-            }
+class Solution {
+    // 1    2   3   4   5   6   7
+    public int lengthOfLongestSubstring(String s) {
+       int ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int right = 0, left = 0; right < s.length(); right ++){
+            char c = s.charAt(right);
+            if(map.containsKey(c)) left = Math.max(left, map.get(c));
+            map.put(c, right + 1);
+            ans = Math.max(ans, right -left + 1);
         }
-        return ans;
+           
+           
+           
+           return ans;
     }
 }
-​
-​
-// correct but time lime exceed with long strings
-/*
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j <= n; j++){
-                if(allUnique(s, i, j)) ans = Math.max(ans, j - i);
-            }
-        }
-        return ans;
-    }
-    
-    boolean allUnique(String s, int start, int end){
-        Set<Character> set = new HashSet<>();
-        for(int i = start; i < end; i++){
-            Character ch = s.charAt(i);
-            if(set.contains(ch)) return false;
-            set.add(ch);
-        }
-        return true;
-    }
-}*/
