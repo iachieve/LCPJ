9b8caf5cf52cfd86ed681f11ddc8097bec6586e8
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-20 09:13:40 -0800

    4 ms

--- solutions/3. Longest Substring Without Repeating Characters.java
+++ solutions/3. Longest Substring Without Repeating Characters.java
@@ -1,17 +1,18 @@
 class Solution {
-    // 1    2   3   4   5   6   7
+    // 1    2   3   4   5   6   7   8   9   10
+    // 1    2   3   4   5   6   
     public int lengthOfLongestSubstring(String s) {
-       int ans = 0;
+      
+        int ans = 0;
         Map<Character, Integer> map = new HashMap<>();
         for(int right = 0, left = 0; right < s.length(); right ++){
             char c = s.charAt(right);
             if(map.containsKey(c)) left = Math.max(left, map.get(c));
             map.put(c, right + 1);
-            ans = Math.max(ans, right -left + 1);
+            ans = Math.max(ans, right - left + 1);
         }
-           
-           
-           
-           return ans;
+        
+        return ans;
+        
     }
 }
