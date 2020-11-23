00e52a51d9f7756729e6e7fdf6ab6b2079d073b1
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-23 00:51:41 -0800

    Update 707. Design Linked List.java
    707. Design Linked List.java

--- solutions/707. Design Linked List.java
+++ solutions/707. Design Linked List.java
@@ -1,119 +1,194 @@
-
-// doubly linked list
-public class ListNode {
+class Node{
+  Node next, prev;
   int val;
-  ListNode next;
-  ListNode prev;
-  ListNode(int x) { val = x; }
+  public Node(int x) { this.val = x;}
 }
-
 class MyLinkedList {
-  int size;
-  // sentinel nodes as pseudo-head and pseudo-tail
-  ListNode head, tail;
-  public MyLinkedList() {
-    size = 0;
-    head = new ListNode(0);
-    tail = new ListNode(0);
-    head.next = tail;
-    tail.prev = head;
-  }
-
-  /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
-  public int get(int index) {
-    // if index is invalid
-    if (index < 0 || index >= size) return -1;
-
-    // choose the fastest way: to move from the head
-    // or to move from the tail
-    ListNode curr = head;
-    if (index + 1 < size - index)
-      for(int i = 0; i < index + 1; ++i) curr = curr.next;
-    else {
-      curr = tail;
-      for(int i = 0; i < size - index; ++i) curr = curr.prev;
+    Node head, tail;
+    int size;
+    public MyLinkedList() {
+        this.size = 0;
+        this.head = new Node(-1);
+        this.tail = new Node(-1);
+        head.next = tail;
+        tail.prev = head;
     }
-
-    return curr.val;
-  }
-
-  /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
-  public void addAtHead(int val) {
-    ListNode pred = head, succ = head.next;
-
-    ++size;
-    ListNode toAdd = new ListNode(val);
-    toAdd.prev = pred;
-    toAdd.next = succ;
-    pred.next = toAdd;
-    succ.prev = toAdd;
-  }
-
-  /** Append a node of value val to the last element of the linked list. */
-  public void addAtTail(int val) {
-    ListNode succ = tail, pred = tail.prev;
-
-    ++size;
-    ListNode toAdd = new ListNode(val);
-    toAdd.prev = pred;
-    toAdd.next = succ;
-    pred.next = toAdd;
-    succ.prev = toAdd;
-  }
-
-  /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
-  public void addAtIndex(int index, int val) {
-    // If index is greater than the length, 
-    // the node will not be inserted.
-    if (index > size) return;
-
-    // [so weird] If index is negative, 
-    // the node will be inserted at the head of the list.
-    if (index < 0) index = 0;
-
-    // find predecessor and successor of the node to be added
-    ListNode pred, succ;
-    if (index < size - index) {
-      pred = head;
-      for(int i = 0; i < index; ++i) pred = pred.next;
-      succ = pred.next;
+    
+    public int get(int idx) {
+        if(idx < 0 || idx >= size) return -1;
+        Node curr;
+        if(idx + 1 < size - idx){
+            curr = head;
+            for(int i = 0; i < idx + 1; i++) curr = curr.next;
+        }else{
+            curr = tail;
+            for(int i = 0; i < size - idx; i++) curr = curr.prev;
+        }
+        return curr.val;
     }
-    else {
-      succ = tail;
-      for (int i = 0; i < size - index; ++i) succ = succ.prev;
-      pred = succ.prev;
+    
+    public void addAtHead(int val) {
+        Node pred = head, succ = head.next, toAdd = new Node(val);
+        toAdd.next = succ;
+        toAdd.prev = pred;
+        succ.prev = toAdd;
+        pred.next = toAdd;
+        ++size;
     }
-
-    // insertion itself
-    ++size;
-    ListNode toAdd = new ListNode(val);
-    toAdd.prev = pred;
-    toAdd.next = succ;
-    pred.next = toAdd;
-    succ.prev = toAdd;
-  }
-
-  /** Delete the index-th node in the linked list, if the index is valid. */
-  public void deleteAtIndex(int index) {
-    // if the index is invalid, do nothing
-    if (index < 0 || index >= size) return;
-
-    // find predecessor and successor of the node to be deleted
-    ListNode pred, succ;
-    if (index < size - index) {
-      pred = head;
-      for(int i = 0; i < index; ++i) pred = pred.next;
-      succ = pred.next.next;
+    
+    public void addAtTail(int val) {
+        Node pred = tail.prev, succ = tail, toAdd = new Node(val);
+        
+        toAdd.next = succ;
+        toAdd.prev = pred;
+        succ.prev = toAdd;
+        pred.next = toAdd;
+            
+        ++size;
     }
-    else {
-      succ = tail;
-      for (int i = 0; i < size - index - 1; ++i) succ = succ.prev;
-      pred = succ.prev.prev;
+    
+    public void addAtIndex(int idx, int val) {
+        if(idx > size) return;
+        if(idx < 0) idx = 0;
+        Node pred, succ, toAdd = new Node(val);
+        if(idx < size - idx){
+            pred = head;
+            for(int i = 0; i < idx; i++) pred = pred.next;
+            succ = pred.next;
+        }else{
+            succ = tail;
+            for(int i = 0; i < size - idx; i++) succ = succ.prev;
+            pred = succ.prev;
+        }
+        
+        ++size;
+        toAdd.next = succ;
+        toAdd.prev = pred;
+        succ.prev = toAdd;
+        pred.next = toAdd;
     }
-
-    // delete pred.next 
-    --size;
-    pred.next = succ;
-    succ.prev = pred;
-  }
+    
+    public void deleteAtIndex(int idx) {
+        if(idx < 0 || idx >= size) return;
+        Node pred, succ;
+        if(idx < size - idx){
+            pred = head;
+            for(int i = 0; i < idx; i++) pred = pred.next;
+            succ = pred.next.next;
+        }else{
+            succ = tail;
+            for(int i = 0; i < size - idx - 1; i++) succ = succ.prev;
+            pred = succ.prev.prev;
+        }
+        
+        --size;
+        pred.next = succ;
+        succ.prev = pred;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
