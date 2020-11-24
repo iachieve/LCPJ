f217640194fa93da0dd9b0f00ba15279cec010a5
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-24 08:48:11 -0800

    0 ms

--- solutions/17. Letter Combinations of a Phone Number.java
+++ solutions/17. Letter Combinations of a Phone Number.java
@@ -1,31 +1,21 @@
 class Solution {
     private static final String[] MAPPING = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    
-  public List<String> letterCombinations(String digits) {
+    public List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<>();
-        if (digits.length() == 0) return res;
-        exploreCombinations(0, new StringBuilder(), digits, res);
+        if(digits.length() == 0) return res;
+        exploreCombination(0, new StringBuilder(), digits, res);
         return res;
     }
-    
-    private void exploreCombinations(int currIdx, StringBuilder partial, String digits, List<String> res) {
-        if (currIdx == digits.length()) {
+    // naming partial or current goes
+    private void exploreCombination(int pos, StringBuilder partial, String digits, List<String> res){
+        if(pos == digits.length()){
             res.add(partial.toString());
             return;
         }
-​
-        String letters = MAPPING[digits.charAt(currIdx) - '0'];
-​
-        for (char possibleLetter: letters.toCharArray()) {
-            // 1.) Choose - Append the letter that this digit can materialize into
-            partial.append(possibleLetter);
-​
-            // 2.) Explore - Recurse on the decision with changed state. We advance the digit we are working on.
-            exploreCombinations(currIdx + 1, partial, digits, res);
-​
-            // 3.) Unchoose - We have returned to this stack frame of choice. Remove the choice, next loop will choose again.
-            partial.deleteCharAt(partial.length() - 1);
+        String letters = MAPPING[digits.charAt(pos) - '0'];
+        for(char c: letters.toCharArray()){
+            exploreCombination(pos + 1, partial.append(c), digits, res);
+            partial.deleteCharAt(partial.length()-1);
         }
     }
-​
 }
