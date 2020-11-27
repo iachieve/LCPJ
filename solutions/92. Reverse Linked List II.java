c06fe2c2cb64e3b282f510d06537a8275506e13e
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-27 05:04:22 -0800

    0 ms

--- solutions/92. Reverse Linked List II.java
+++ solutions/92. Reverse Linked List II.java
@@ -1,37 +1,15 @@
 class Solution {
-    public ListNode reverseBetween(ListNode head, int start, int end) {
-      if(head == null) return head;
-        ListNode dummyHead = new ListNode(-1);
-        dummyHead.next = head;
-        ListNode prev = dummyHead;
-        for(int i=1; i < start; i++) prev = prev.next;
-        ListNode first = prev.next;
-        while(start < end){
-            ListNode second = first.next;
-            first.next = second.next;
-            second.next = prev.next;
-            prev.next = second;
-            start++;
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummy = new ListNode(-1), pre = dummy;
+        dummy.next = head;
+        for(int i = 1; i < m; i++) pre = pre.next;
+        ListNode curr = pre.next;
+        while(m++ < n){
+            ListNode temp = curr.next;
+            curr.next = temp.next;
+            temp.next = pre.next;
+            pre.next = temp;
         }
-        return dummyHead.next;
+        return dummy.next;
     }
 }
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
