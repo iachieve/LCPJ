c2e921d392e7355a91d12b4a6111189100ec29d3
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-28 13:40:07 -0800

    0 ms

--- solutions/876. Middle of the Linked List.java
+++ solutions/876. Middle of the Linked List.java
@@ -1,9 +1,9 @@
-class Solution {
+class Solution { // 
     public ListNode middleNode(ListNode head) {
        ListNode slow = head, fast = head;
         while(fast != null && fast.next != null){
-            slow = slow.next;
             fast = fast.next.next;
+            slow = slow.next;
         }
         return slow;
     }
