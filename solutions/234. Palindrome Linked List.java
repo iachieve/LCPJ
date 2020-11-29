6d44221b1780f779e84a74d51b8c4000ed4c8466
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-29 10:20:29 -0800

    Update 234. Palindrome Linked List.java
    234. Palindrome Linked List.java

--- solutions/234. Palindrome Linked List.java
+++ solutions/234. Palindrome Linked List.java
@@ -8,6 +8,54 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// solution #1: 
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> vals = new ArrayList<>();
+
+        // Convert LinkedList into ArrayList.
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        // Use two-pointer technique to check for palindrome.
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            // Note that we must use ! .equals instead of !=
+            // because we are comparing Integer, not int.
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+}
+
+// #solution #2
+class Solution {
+
+    private ListNode frontPointer;
+
+    private boolean recursivelyCheck(ListNode currentNode) {
+        if (currentNode != null) {
+            if (!recursivelyCheck(currentNode.next)) return false;
+            if (currentNode.val != frontPointer.val) return false;
+            frontPointer = frontPointer.next;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        frontPointer = head;
+        return recursivelyCheck(head);
+    }
+}
 ​
 class Solution {
     // two pointers technique (slow and fast)
@@ -57,3 +105,7 @@ class Solution {
         return newHead;
     }
 }
+
+
+
+
