db0897a01ec15e067bd67137c36d7fd2a9af9407
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-30 03:23:14 -0800

    7 ms

--- solutions/37. Sudoku Solver.java
+++ solutions/37. Sudoku Solver.java
@@ -2,15 +2,14 @@ class Solution {
     public void solveSudoku(char[][] board) {
         solve(board);
     }
-    
-    private boolean solve(char[][] board) {
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                if (board[r][c] != '.') continue;
-                for (char num = '1'; num <= '9'; num++) {
-                    if (isValid(board, r, c, num)) {
-                        board[r][c] = num;
-                        if (solve(board)) return true;
+    private boolean solve(char[][] board){
+        for(int r = 0; r < 9; r++){
+            for(int c =0; c < 9; c++){
+                if(board[r][c] != '.') continue;
+                for(char pos = '1'; pos <= '9'; pos++){
+                    if(valid(board, r, c, pos)){
+                        board[r][c] = pos;
+                        if(solve(board)) return true;
                         else board[r][c] = '.';
                     }
                 }
@@ -18,18 +17,17 @@ class Solution {
             }
         }
         return true;
-    }
-​
-    private boolean isValid(char[][] board, int row, int col, char val) {
+    }
+    
+    private boolean valid(char[][] board, int r, int c, char pos) {
         for (int i = 0; i < 9; i++)
-            if (board[i][col] == val || board[row][i] == val) return false;
+            if (board[i][c] == pos || board[r][i] == pos) return false;
 ​
-        int topLeftRow = row - row % 3;
-        int topLeftCol = col - col % 3;
+        int topLeftRow = r - r % 3, topLeftCol = c - c % 3;
 ​
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
-                if (val == board[topLeftRow + i][topLeftCol + j])
+                if (pos == board[topLeftRow + i][topLeftCol + j])
                     return false;
 ​
         return true;
