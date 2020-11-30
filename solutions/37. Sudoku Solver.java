514eec0d6f9d09d5ea41981a7c05aa6aff319260
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-29 17:11:22 -0800

    5 ms

--- solutions/37. Sudoku Solver.java
+++ solutions/37. Sudoku Solver.java
@@ -3,9 +3,7 @@ class Solution {
         solvedFromCell(0, 0, board);
     }
     
-    
-​
-    private static  boolean solvedFromCell(int row, int col, char[][] board) {
+    private boolean solvedFromCell(int row, int col, char[][] board) {
         if (col == 9) {
             col = 0;
             row++;
@@ -17,7 +15,7 @@ class Solution {
 ​
         for (int n = 1; n <= 9; n++) {
             char val = (char) (n + '0');
-            if (canPlaceValue(board, row, col, val)) {
+            if (valid(board, row, col, val)) {
                 board[row][col] = val;
 ​
                 if (solvedFromCell(row, col + 1, board)) {
@@ -31,18 +29,18 @@ class Solution {
         return false;
     }
 ​
-  private static  boolean canPlaceValue(char[][] board, int row, int col, char val) {
+    private boolean valid(char[][] board, int row, int col, char val) {
         for (int i = 0; i < 9; i++)
             if(board[i][col] == val || board[row][i] == val) return false;
 ​
-        int topLeftRow = row - row % 3;
-        int topLeftCol = col - col % 3;
+        int topLeftRow = 3 * (row / 3);
+        int topLeftCol = 3 * (col / 3);
 ​
-        for (int i = 0; i < 3; i++) 
-            for (int j = 0; j < 3; j++) 
-                if (val == board[topLeftRow + i][topLeftCol + j]) 
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (val == board[topLeftRow + i][topLeftCol + j])
                     return false;
-                
+​
         return true;
     }
 ​
