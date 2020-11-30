79ffcc26683c7dc171766688a8dbab582a16c4e3
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-11-30 05:22:20 -0800

    4 ms

--- solutions/51. N-Queens.java
+++ solutions/51. N-Queens.java
@@ -6,38 +6,34 @@ class Solution {
         dfs(res, new int[n], 0, n);
         return res;
     }
-    
-     //pos[i]  mean we choose pos[i] in line i. 
-     //(i , pos[i])
-    void dfs(List<List<String>> res, int[] pos, int index, int n) {
+​
+    void dfs(List<List<String>> res, int[] path, int idx, int n) {
         for (int i = 0; i < n; i++) {
-            pos[index] = i;
-            if (checkLegal(pos, index, i)) {
-                if (index + 1 == n) {
-                    List<String> t = new LinkedList<>();
-                    for (int j = 0; j < n; j++) {
-                        StringBuilder sb = new StringBuilder();
-                        for (int k = 0; k < n; k++) {
-                            sb.append(k == pos[j] ? "Q" : ".");
-                        }
-                        t.add(sb.toString());
-                    }
-                    res.add(t);
-                } else {
-                    dfs(res, pos, index + 1, n);
-                }
+            path[idx] = i;
+            if (isValid(path, idx)) {
+                if (idx + 1 == n) addPath(res, path, n);
+                else dfs(res, path, idx + 1, n);
             }
         }
     }
 ​
-    boolean checkLegal(int[] pos, int index, int i) {
-        for (int j = 0; j < index; j++) {
-            if (pos[index] == pos[j]
-                    || Math.abs(index - j) == Math.abs(pos[index] - pos[j])) {
-                return false;
-            }
+    private void addPath(List<List<String>> res, int[] path, int n) {
+        List<String> t = new LinkedList<>();
+        for (int i = 0; i < n; ++i) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++)
+                sb.append(j == path[i] ? "Q" : ".");
+            t.add(sb.toString());
         }
+        res.add(t);
+    }
+​
+    boolean isValid(int[] path, int idx) {
+        for (int i = 0; i < idx; ++i)
+            if (path[idx] == path[i] || idx - i == Math.abs(path[idx] - path[i]))
+                return false;
         return true;
     }
 ​
 }
+​
