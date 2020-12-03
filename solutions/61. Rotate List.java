aa263646e763999f28278a855c735847e7f25030
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-03 05:26:11 -0800

    0 ms

--- solutions/61. Rotate List.java
+++ solutions/61. Rotate List.java
@@ -1,13 +1,15 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-       if(head == null || head.next == null) return head;
-        ListNode oldTail = head, newTail = head;
+        if(head == null || head.next == null) return head;
+        ListNode oldTail = head, newTail = head, newHead = null;
         int len;
-        for (len = 1; oldTail.next != null; len++) oldTail = oldTail.next;
-        for(int i = 0; i < len - k % len - 1; i++) newTail = newTail.next;
+        for (len = 1; oldTail.next != null; ++len) oldTail = oldTail.next;
+        for (int i = 0; i < len - k % len - 1; ++i) newTail = newTail.next;
         oldTail.next = head;
-        ListNode newHead = newTail.next;
+        newHead = newTail.next;
         newTail.next = null;
         return newHead;
     }
 }
+​
+//  1   2   3   4   5   6   7   8
