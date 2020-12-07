ba6f6a4e2542f5b559a45660e64be336b78e2bd6
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-07 06:29:54 -0800

    7 ms

--- solutions/120. Triangle.java
+++ solutions/120. Triangle.java
@@ -1,16 +1,23 @@
-public class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
+class Solution {
+    public int minimumTotal(List<List<Integer>> t) {
+        int n = t.size();
+        int[] dp = new int[n];
+        for(int i = 0; i < n; i++) dp[i] = t.get(n-1).get(i);
+        System.out.println(Arrays.toString(dp));
         
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) 
-            arr[i] = triangle.get(n - 1).get(i);
-        
-        // bottom up comparion
-        for (int i = n - 2; i >= 0; i--) 
-            for (int j = 0; j <= i; j++) 
-                arr[j] = Math.min(arr[j], arr[j + 1]) + triangle.get(i).get(j);
-        
-        return arr[0];
+        // iterate over 
+        for(int tRow = n-2; tRow >= 0; tRow--){
+            for(int tCol = 0; tCol <= tRow; tCol++){
+                int tColVal = dp[tCol];
+                int tColNextVal = dp[tCol+1];
+                int min = Math.min(tColVal, tColNextVal);
+                int tVal = t.get(tRow).get(tCol);
+                dp[tCol] = min + tVal;
+                // System.out.println(Arrays.toString(dp));
+            }
+            System.out.println(Arrays.toString(dp));
+​
+        }
+        return dp[0];
     }
 }
