8fa15af994968dc909b3e68cc6f69533acaa38c1
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-07 04:50:05 -0800

    0 ms

--- solutions/62. Unique Paths.java
+++ solutions/62. Unique Paths.java
@@ -1,17 +1,14 @@
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Unique Paths.
+// Memory Usage: 35.9 MB, less than 42.10% of Java online submissions for Unique Paths.
 class Solution {
-  public int uniquePaths(int m, int n) {
-    int[][] d = new int[m][n];
-​
-    for(int row = 0; row < m; ++row) {
-      for(int col = 0; col < n; ++col) {
-          if(row == 0 || col == 0)
-              d[row][col] = 1;
-          
-          else
-              
-        d[row][col] = d[row - 1][col] + d[row][col - 1];
-      }
-    }
-    return d[m - 1][n - 1];
-  }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int row = 0; row < m; ++row){
+            for (int col = 0; col < n; ++col) {
+                if(row == 0 || col == 0) dp[row][col] = 1;
+                else dp[row][col] = dp[row-1][col] + dp[row][col-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
 }
