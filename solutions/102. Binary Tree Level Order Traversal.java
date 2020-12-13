b32ee4b0830a63ec1cbdb8614146aca71817d383
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-12 16:23:23 -0800

    0 ms

--- solutions/102. Binary Tree Level Order Traversal.java
+++ solutions/102. Binary Tree Level Order Traversal.java
@@ -1,26 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> levels = new ArrayList<List<Integer>>();
-        if (root == null) return levels;
-​
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
         Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-​
+        q.add(root);
         while (!q.isEmpty()) {
-            List<Integer> currLevel = new ArrayList<>();
-​
-            int levelSize = q.size();
-            for (int i = 0; i < levelSize; i++) {
+            List<Integer> layer = new ArrayList<>();
+            int qSize = q.size();
+            for (int i = 0; i < qSize; i++) {
                 TreeNode currNode = q.poll();
-                currLevel.add(currNode.val);
-                if (currNode.left != null) q.offer(currNode.left);
-​
-                if (currNode.right != null) q.offer(currNode.right);
+                layer.add(currNode.val);
+                if (currNode.left != null) q.add(currNode.left);
+                if(currNode.right != null) q.add(currNode.right);
             }
-​
-            levels.add(currLevel);
-        }
-​
-        return levels;
+            res.add(layer);            
+        }
+        return res;
     }
 }
