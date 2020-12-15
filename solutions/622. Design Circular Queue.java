14dc1a7262248021551f90c3cd61b5b78f729a04
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-14 16:05:56 -0800

    4 ms

--- solutions/622. Design Circular Queue.java
+++ solutions/622. Design Circular Queue.java
@@ -1,43 +1,58 @@
 class MyCircularQueue {
 ​
-    int[] a;
-    int len = 0, front = 0, rear = -1;
-    
-    public MyCircularQueue(int k) {
-        a = new int[k];
-    }
-    
-    public boolean enQueue(int value) {
-        if(isFull()) return false;
-        len++;
-         rear = (rear + 1) % a.length;
-         a[rear] = value;
-        return true;
-    }
-    
-    public boolean deQueue() {
-        if(isEmpty()) return false;
-        front = (front + 1) % a.length;
-        len--;
-         return true;
-    }
-    
-    public int Front() { return isEmpty() ? -1 : a[front];}
-    
-    public int Rear() {return isEmpty() ? -1 : a[rear];}
-    
-    public boolean isEmpty() { return len == 0; }
-    
-    public boolean isFull() { return len == a.length; }
-}
+  private int[] queue;
+  private int headIndex;
+  private int count;
+  private int capacity;
 ​
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue obj = new MyCircularQueue(k);
- * boolean param_1 = obj.enQueue(value);
- * boolean param_2 = obj.deQueue();
- * int param_3 = obj.Front();
- * int param_4 = obj.Rear();
- * boolean param_5 = obj.isEmpty();
- * boolean param_6 = obj.isFull();
- */
+  /** Initialize your data structure here. Set the size of the queue to be k. */
+  public MyCircularQueue(int k) {
+    this.capacity = k;
+    this.queue = new int[k];
+    this.headIndex = 0;
+    this.count = 0;
+  }
+​
+  /** Insert an element into the circular queue. Return true if the operation is successful. */
+  public boolean enQueue(int value) {
+    if (this.count == this.capacity)
+      return false;
+    this.queue[(this.headIndex + this.count) % this.capacity] = value;
+    this.count += 1;
+    return true;
+  }
+​
+  /** Delete an element from the circular queue. Return true if the operation is successful. */
+  public boolean deQueue() {
+    if (this.count == 0)
+      return false;
+    this.headIndex = (this.headIndex + 1) % this.capacity;
+    this.count -= 1;
+    return true;
+  }
+​
+  /** Get the front item from the queue. */
+  public int Front() {
+    if (this.count == 0)
+      return -1;
+    return this.queue[this.headIndex];
+  }
+​
+  /** Get the last item from the queue. */
+  public int Rear() {
+    if (this.count == 0)
+      return -1;
+    int tailIndex = (this.headIndex + this.count - 1) % this.capacity;
+    return this.queue[tailIndex];
+  }
+​
+  /** Checks whether the circular queue is empty or not. */
+  public boolean isEmpty() {
+    return (this.count == 0);
+  }
+​
+  /** Checks whether the circular queue is full or not. */
+  public boolean isFull() {
+    return (this.count == this.capacity);
+  }
+}
