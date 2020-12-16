1e5c7d8b385e47be790ba753d439331ac4cc4a6d
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-16 01:50:37 -0800

    1 ms

--- solutions/921. Minimum Add to Make Parentheses Valid.java
+++ solutions/921. Minimum Add to Make Parentheses Valid.java
@@ -1,11 +1,14 @@
 class Solution {
-    public int minAddToMakeValid(String s) {
+    public int minAddToMakeValid(String S) {
         Stack<Character> stack = new Stack<>();
-        for(int i=0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(c == '(')  stack.push(c);
-            else if(c == ')' && !stack.isEmpty() && stack.peek() == '(') stack.pop();
-            else stack.push(c);
+        for (char c: S.toCharArray()) {
+            if (c == '('){
+                stack.push(c);
+            }else if (c == ')' && !stack.isEmpty() && stack.peek() == '('){
+                stack.pop();
+            } else if (c == ')'){
+                stack.push(c);
+            }
         }
         return stack.size();
     }
