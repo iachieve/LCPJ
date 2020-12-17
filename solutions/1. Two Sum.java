4a6a2a412b9986c4356096d98bc4e10fdf0ce0ce
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-17 05:19:46 -0800

    0 ms

---
+++ solutions/1. Two Sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(nums[0], 0);
+        for (int i = 1; i < nums.length; ++i) {
+                int delta = target - nums[i];
+                if (map.containsKey(delta)) {
+                   return new int[] {map.get(delta), i};
+                } else{
+                    map.put(nums[i], i);
+                }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
