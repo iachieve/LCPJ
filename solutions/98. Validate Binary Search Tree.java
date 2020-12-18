f2fe2e52bcb44692286e845e3ebffb7cdae1e506
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-18 03:52:25 -0800

    98. Validate Binary Search Tree.java
    98. Validate Binary Search Tree.java

--- solutions/98. Validate Binary Search Tree.java
+++ solutions/98. Validate Binary Search Tree.java
@@ -28,3 +28,88 @@ class Solution {
         return true;
   }
 }
+
+
+class Solution {
+
+    private Deque<TreeNode> stack = new LinkedList();
+    private Deque<Integer> upperLimits = new LinkedList();
+    private Deque<Integer> lowerLimits = new LinkedList();
+
+    public void update(TreeNode root, Integer low, Integer high) {
+        stack.add(root);
+        lowerLimits.add(low);
+        upperLimits.add(high);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        Integer low = null, high = null, val;
+        update(root, low, high);
+
+        while (!stack.isEmpty()) {
+            root = stack.poll();
+            low = lowerLimits.poll();
+            high = upperLimits.poll();
+
+            if (root == null) continue;
+            val = root.val;
+            if (low != null && val <= low) {
+                return false;
+            }
+            if (high != null && val >= high) {
+                return false;
+            }
+            update(root.right, val, high);
+            update(root.left, low, val);
+        }
+        return true;
+    }
+}
+
+class Solution {
+    // We use Integer instead of int as it supports a null value.
+    private Integer prev;
+    
+    public boolean isValidBST(TreeNode root) {
+        prev = null;
+        return inorder(root);
+    }
+    
+    private boolean inorder(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!inorder(root.left)) {
+            return false;
+        }
+        if (prev != null && root.val <= prev) {
+            return false;
+        }
+        prev = root.val;
+        return inorder(root.right);
+    }
+}
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        Integer prev = null;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            // If next element in inorder traversal
+            // is smaller than the previous one
+            // that's not BST.
+            if (prev != null && root.val <= prev) {
+                return false;
+            }
+            prev = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
