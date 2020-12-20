51927c78e6677cf6f7f61e3e2d76cff0eb72fce8
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-20 06:44:40 -0800

    0 ms

--- solutions/104. Maximum Depth of Binary Tree.java
+++ solutions/104. Maximum Depth of Binary Tree.java
@@ -1,3 +1,10 @@
+// class Solution {
+//     public int maxDepth(TreeNode root) {
+//         if (root == null) return 0;
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
+//         int depth = 0;
+//         while (!queue.isEmpty()) {
 //             int qSize = queue.size();
 //             for (int i = 0; i < qSize; i++) {
 //                 root = queue.poll();
@@ -10,7 +17,7 @@
 //     }
 // }
 ​
-/*
+​
 // recursive
 class Solution {
      public int maxDepth(TreeNode root) {
@@ -18,39 +25,19 @@ class Solution {
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
      }
 }
-*/
+​
 // recursive
-class Solution {
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
-66
-67
-68
-69
-70
-71
-72
-73
-74
-75
-76
-77
-78
-79
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
