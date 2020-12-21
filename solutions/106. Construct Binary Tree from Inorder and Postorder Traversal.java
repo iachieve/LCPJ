99b8e620b6f7fabe8dc1b8ff56337a172ab4192f
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-21 06:28:17 -0800

    106. Construct Binary Tree from Inorder and Postorder Traversal.java
    106. Construct Binary Tree from Inorder and Postorder Traversal.java

--- solutions/106. Construct Binary Tree from Inorder and Postorder Traversal.java
+++ solutions/106. Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -16,3 +16,34 @@ class Solution {
         return root;
     }
 }
+
+
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder.length == 0 || postorder.length == 0) return null;
+        int pp = postorder.length - 1;
+        int ip = inorder.length - 1;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode root = new TreeNode(postorder[pp]);
+        stack.push(root);
+        pp--;
+        TreeNode prev = null;
+        while (pp >= 0) {
+            while (!stack.isEmpty() && stack.peek().val == inorder[ip]) {
+                prev = stack.pop();
+                ip--;
+            }
+            TreeNode newNode = new TreeNode(postorder[pp]);
+            if (prev != null) { 
+                prev.left = newNode;
+            } else {
+                TreeNode currTop = stack.pop();
+                currTop.right = newNode;
+            }
+            stack.push(newNode);
+            prev = null;
+            pp--;
+        }
+        return root;
+    }
+}
