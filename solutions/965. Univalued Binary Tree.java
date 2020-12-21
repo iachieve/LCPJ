de86e9f8958e6b6c8c6663af35344ea285870358
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-21 08:14:09 -0800

    965. Univalued Binary Tree.java
    965. Univalued Binary Tree.java

--- solutions/965. Univalued Binary Tree.java
+++ solutions/965. Univalued Binary Tree.java
@@ -28,3 +28,11 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        boolean left = (root.left == null || (root.left.val == root.val) && isUnivalTree(root.left));
+        boolean right = (root.right == null || (root.right.val == root.val) && isUnivalTree(root.right));
+        return left && right;
+    }
+}
