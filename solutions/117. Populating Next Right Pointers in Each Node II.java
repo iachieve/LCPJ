c3317ff21f0695f6a133fbb3ccd92f04f247e829
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-22 06:30:04 -0800

    0 ms

---
+++ solutions/117. Populating Next Right Pointers in Each Node II.java
@@ -0,0 +1,24 @@
+​
+class Solution {
+    public Node connect(Node root) {
+        Node head = root, dummy = new Node();
+        while (head != null) {
+            Node cur = dummy;
+                while (head != null) {
+                    
+                    if (head.left != null) {
+                        cur.next = head.left;
+                        cur = cur.next;
+                    }
+                    if (head.right != null) {
+                        cur.next = head.right;
+                        cur = cur.next;
+                    }
+                    head = head.next;
+                }
+            head = dummy.next;
+            dummy.next = null;
+        }
+        return root;
+    }
+}
