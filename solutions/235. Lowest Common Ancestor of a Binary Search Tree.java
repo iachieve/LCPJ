2f81e67e1f54051e1a185a1654eeff3f3b5f3998
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-22 07:03:44 -0800

    235. Lowest Common Ancestor of a Binary Search Tree.java
    235. Lowest Common Ancestor of a Binary Search Tree.java

--- solutions/235. Lowest Common Ancestor of a Binary Search Tree.java
+++ solutions/235. Lowest Common Ancestor of a Binary Search Tree.java
@@ -16,3 +16,32 @@ class Solution {
 ​
     }
 }
+
+
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int pVal = p.val;
+        int qVal = q.val;
+        TreeNode node = root;
+
+        // Traverse the tree
+        while (node != null) {
+
+            // Value of ancestor/parent node.
+            int parentVal = node.val;
+
+            if (pVal > parentVal && qVal > parentVal) {
+                // If both p and q are greater than parent
+                node = node.right;
+            } else if (pVal < parentVal && qVal < parentVal) {
+                // If both p and q are lesser than parent
+                node = node.left;
+            } else {
+                // We have found the split point, i.e. the LCA node.
+                return node;
+            }
+        }
+        return null;
+    }
+}
