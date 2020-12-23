454cd129bad169eebb6a188629e899c8231cb1b6
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-23 12:06:30 -0800

    105. Construct Binary Tree from Preorder and Inorder Traversal.java
    105. Construct Binary Tree from Preorder and Inorder Traversal.java

--- solutions/105. Construct Binary Tree from Preorder and Inorder Traversal.java
+++ solutions/105. Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -1,16 +1,16 @@
 class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer, Integer> inMap = new HashMap<>();
-        for (int i = 0; i < inorder.length; ++i) inMap.put(inorder[i], i);
-        return buildTree(preorder, inMap, 0, inorder.length - 1, 0);
-    }
-    public TreeNode buildTree(int[] pre, Map<Integer, Integer> inMap, int inStart, int inEnd, int rootIndex) {
-        if (inStart > inEnd) return null;
-        TreeNode root = new TreeNode(pre[rootIndex]);
-        int inMid = inMap.get(pre[rootIndex]);
-        root.left = buildTree(pre, inMap, inStart, inMid - 1, rootIndex + 1);
-        int leftTreeLength = inMid - inStart;
-        root.right = buildTree(pre, inMap, inMid + 1, inEnd, rootIndex + leftTreeLength + 1);
-        return root;
-    }
+    public TreeNode buildTree(int[] pre, int[] in) {
+        Map<Integer, Integer> inMap = new HashMap<>();
+        for (int i = 0; i < in.length; ++i) inMap.put(in[i], i);
+        return buildTree(pre, inMap, 0, in.length - 1, 0);
+    }
+    public TreeNode buildTree(int[] pre, Map<Integer, Integer> inMap, int inStart, int inEnd, int rootIndex) {
+        if (inStart > inEnd) return null;
+        TreeNode root = new TreeNode(pre[rootIndex]);
+        int inMid = inMap.get(pre[rootIndex]);
+        root.left = buildTree(pre, inMap, inStart, inMid - 1, rootIndex + 1);
+        int leftTreeLen = inMid - inStart;
+        root.right = buildTree(pre, inMap, inMid + 1, inEnd, rootIndex + leftTreeLen + 1);
+        return root;
+    }
 }
