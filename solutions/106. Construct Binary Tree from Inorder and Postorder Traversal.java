afbc9a01d4dbf767266a61aa500c6d3fb8749cd9
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-23 11:57:21 -0800

    1 ms

--- solutions/106. Construct Binary Tree from Inorder and Postorder Traversal.java
+++ solutions/106. Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -1,49 +1,20 @@
 class Solution {
-    int index;
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        Map<Integer, Integer> inorderMap = new HashMap<>();
-        for(int i=0; i<inorder.length; i++) inorderMap.put(inorder[i], i);
-        index = postorder.length-1;
-        return build(postorder, inorderMap, 0, postorder.length-1);
+public TreeNode buildTree(int[] inorder, int[] postorder) {
+        Map<Integer, Integer> inOrderMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) inOrderMap.put(inorder[i], i);
+        return buildTree(postorder, inOrderMap, 0, inorder.length-1, postorder.length - 1);
     }
-    
-    public TreeNode build(int[] postorder, Map<Integer, Integer> inorderMap, int start, int end){
-        if(start > end) return null;
-        TreeNode root = new TreeNode(postorder[index--]);
-        int pivot = inorderMap.get(root.val);
-        root.right = build(postorder, inorderMap, pivot + 1, end);
-        root.left = build(postorder, inorderMap, start, pivot - 1);
+​
+    private TreeNode buildTree(int[] postorder, Map<Integer, Integer> inOrderMap, int inStart, int inEnd, int rootIndex) {
+        if (inStart > inEnd) return null;
+        TreeNode root = new TreeNode(postorder[rootIndex]);
+        int inOrderMidIndex = inOrderMap.get(postorder[rootIndex]);
+        
+        root.right = buildTree(postorder, inOrderMap, inOrderMidIndex+1, inEnd, rootIndex-1);
+        
+        int rightTreeLength = inEnd - inOrderMidIndex;
+        root.left = buildTree(postorder, inOrderMap, inStart, inOrderMidIndex-1, rootIndex-rightTreeLength-1);
+        
         return root;
     }
-}
-
-
-class Solution {
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if (inorder.length == 0 || postorder.length == 0) return null;
-        int pp = postorder.length - 1;
-        int ip = inorder.length - 1;
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode root = new TreeNode(postorder[pp]);
-        stack.push(root);
-        pp--;
-        TreeNode prev = null;
-        while (pp >= 0) {
-            while (!stack.isEmpty() && stack.peek().val == inorder[ip]) {
-                prev = stack.pop();
-                ip--;
-            }
-            TreeNode newNode = new TreeNode(postorder[pp]);
-            if (prev != null) { 
-                prev.left = newNode;
-            } else {
-                TreeNode currTop = stack.pop();
-                currTop.right = newNode;
-            }
-            stack.push(newNode);
-            prev = null;
-            pp--;
-        }
-        return root;
-    }
 }
