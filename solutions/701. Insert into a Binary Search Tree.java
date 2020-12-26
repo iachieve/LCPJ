697fe10e08a168064d05ebd5cd5ea83af0ac8f1e
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-26 06:40:58 -0800

    0 ms

--- solutions/701. Insert into a Binary Search Tree.java
+++ solutions/701. Insert into a Binary Search Tree.java
@@ -1,37 +1,48 @@
-class Solution {
-    
-    // recursive solution   
+/*
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root == null) return new TreeNode(val);
-        if(val > root.val) root.right = insertIntoBST(root.right, val);
-        else root.left = insertIntoBST(root.left, val);
-        return root;
-    }
-    
-    /*
-    // iterative solution
-    public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root == null) return new TreeNode(val);
-        
-        TreeNode currNode = root;
-        while(true){
-            if(currNode.val <= val){
-                if(currNode.right != null){
-                    currNode = currNode.right;
-                }else{
-                    currNode.right = new TreeNode(val);
-                    break;
-                }
-            }else{
-                if(currNode.left != null){
-                    currNode = currNode.left;
-                } else{
-                    currNode.left = new TreeNode(val);
-                    break;
-                }
-            }
+        if (root == null) return new TreeNode(val);
+        if (root.val > val) {
+            if (root.left == null) root.left = new TreeNode(val);
+            else insertIntoBST(root.left, val);
+        }
+        else if (root.val < val) {
+            if (root.right == null) root.right = new TreeNode(val);
+            else insertIntoBST(root.right, val);
         }
         return root;
     }
-    */
+*/
+​
+/*
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (root.val > val) root.left = insertIntoBST(root.left, val);
+        else root.right = insertIntoBST(root.right, val);
+        return root;
+    }
+*/
+​
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+         TreeNode node = root;
+        while (node != null) {
+            if (val > node.val) {
+                if (node.right == null) {
+                    node.right = new TreeNode(val);
+                    return root;
+                }else node = node.right;
+            }else{
+                if (node.left == null) {
+                    node.left = new TreeNode(val);
+                    return root;
+                }else node = node.left;
+            }
+        }
+        return new TreeNode(val);
+    }
 }
+​
+​
+​
+​
+​
