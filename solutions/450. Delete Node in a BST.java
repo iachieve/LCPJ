3b57397d5577f3bab898f1f8fd725f00b00d0bbf
Mohamed Abdelhalim <mhalim80@hotmail.com>
2020-12-26 07:11:09 -0800

    0 ms

--- solutions/450. Delete Node in a BST.java
+++ solutions/450. Delete Node in a BST.java
@@ -1,21 +1,42 @@
 class Solution {
-    public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null) return null;
-        
-        if(root.val > key)
-            root.left = deleteNode(root.left, key);
-        else if(root.val < key)
-            root.right = deleteNode(root.right, key);
-        else{
-            if(root.left == null)
-                return root.right;
-            if(root.right == null)
-                return root.left;
-            TreeNode rightMin = root.right;
-            while(rightMin.left != null) rightMin = rightMin.left;
-            rightMin.left = root.left;
-            return root.right;
+    /**
+     * findSuccesor - Helper function for finding successor
+     * In BST, succesor of root is the leftmost child in root's right subtree
+     */
+    private TreeNode findSuccessor(TreeNode root) {
+        TreeNode cur = root.right;
+        while (cur != null && cur.left != null) {
+            cur = cur.left;
         }
-        return root;
+        return cur;
     }
-}
+    public TreeNode deleteNode(TreeNode root, int key) {
+        // return null if root is null
+        if (root == null) {
+            return root;
+        }
+​
+        // delete current node if root is the target node
+        if (root.val == key) {
+            // replace root with root->right if root->left is null  
+            if (root.left == null) {
+                return root.right;
+            }
+​
+            // replace root with root->left if root->right is null
+            if (root.right == null) {
+                return root.left;
+            }
+​
+            // replace root with its successor if root has two children
+            TreeNode p = findSuccessor(root);
+            root.val = p.val;
+            root.right = deleteNode(root.right, p.val);
+            return root;
+        }
+        if (root.val < key) {
+            // find target in right subtree if root->val < key
+            root.right = deleteNode(root.right, key);
+        } else {
+            // find target in left subtree if root->val > key
+            root.left = deleteNode(root.left, key);
